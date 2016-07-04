3d7bbe49ee2c1058517ef6e2bd94103932d21bee
NoisYmenT <noisyment@gmail.com>
2016-07-05 00:20:09 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -48,7 +48,7 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "Molly Bot",
+      botName: "Lit Bot",
       language: "english",
       chatLink: "https://rawgit.com/NoisYmenT/custom/custommaster/lang/en.json",
       scriptLink: "https://rawgit.com/NoisYmenT/source/sourcemaster/basicBot.js",
